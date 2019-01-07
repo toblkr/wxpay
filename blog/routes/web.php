746119a6696ee15809859439541a582d8e1f3d4f
toblkr <yuzhoukr@gmail.com>
2019-01-07 17:37:46 +0800

    route added 7 Jan

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -19,15 +19,61 @@ Route::get('/',function(){
     return 'Index page';
 });
 
-Route::get('/help',function(){
-    return 'Help Page';
+Route::get('/help', function() {
+    return route('bar1');
 });
+
+Route::get('/bar', function() {
+    //
+})->name('bar1');
 
 Route::match(['get', 'post'],'/foo', function () {
     // 该路由将匹配 get 和 post 方法的 'foo' url
 });
 
 
-Route::get('name/{n1}/{n2}',function($name1,$name2){//按顺序传输给,$name1 $name2
-    return 'I`m ' .$name2;
+Route::get('hello/{name?}', function ($name = null) {
+    return 'Hello! '.$name;
 });
+
+Route::any('/help1', function() {
+    // 该路由将匹配 所有 类型的 'foo' url
+})->name('any');
+
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/',function(){
+
+    });
+    Route::get('name',function(){
+
+    });
+});
+
+Route::group(['namespace' => 'Admin'], function()
+{
+    // 控制器在「App\Http\Controllers\Admin」命名空间
+
+    Route::group(['namespace' => 'User'], function()
+    {
+        // 控制器在「App\Http\Controllers\Admin\User」命名空间
+    });
+});
+
+
+//前缀
+Route::group(['prefix' => 'user'], function () {
+    Route::get('name', function ()    {
+        //
+    });
+    Route::get('age', function ()    {
+        //
+    });
+    Route::get('introduction', function ()    {
+        //
+    });
+});
+
+Route::get('hello/{name?}', function ($name = 'Tom') {
+    return 'Hello! '.$name;
+})->where('name','[A-Za-z]+');
